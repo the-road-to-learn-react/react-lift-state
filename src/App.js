0ce00364858b37f5b102d84950af0bce2616021f
Robin Wieruch <wrobin@gmx.net>
2019-05-23 08:53:27 +0200

    lift state up

--- src/App.js
+++ src/App.js
@@ -7,12 +7,28 @@ const MYLIST = [
 
 const App = () => <SearchableList list={MYLIST} />;
 
-const SearchableList = ({ list }) => (
-  <div>
-    <Search>Search List:</Search>
-    <List list={list} />
-  </div>
-);
+const SearchableList = ({ list }) => {
+  const [query, setQuery] = React.useState('');
+
+  const handleQuery = event => {
+    setQuery(event.target.value);
+  };
+
+  const filteredList = !query
+    ? list
+    : list.filter(item =>
+        item.name.toLowerCase().includes(query.toLowerCase())
+      );
+
+  return (
+    <div>
+      <Search query={query} handleQuery={handleQuery}>
+        Search List:
+      </Search>
+      <List list={filteredList} />
+    </div>
+  );
+};
 
 const List = ({ list }) => (
   <ul>
@@ -22,19 +38,11 @@ const List = ({ list }) => (
   </ul>
 );
 
-const Search = ({ children }) => {
-  const [query, setQuery] = React.useState('');
-
-  const handleChange = event => {
-    setQuery(event.target.value);
-  };
-
-  return (
-    <div>
-      {children}
-      <input type="text" value={query} onChange={handleChange} />
-    </div>
-  );
-};
+const Search = ({ query, handleQuery, children }) => (
+  <div>
+    {children}
+    <input type="text" value={query} onChange={handleQuery} />
+  </div>
+);
 
 export default App;
